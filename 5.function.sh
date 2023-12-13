5094eed9eaa2a0d5d0dffd71c03850de6d882802
Anil-Nadikuda <anilnadikuda1430@gmail.com>
2023-12-13 22:14:05 +0530

    changes done

---
+++ 5.function.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+ID=$(id -u)
+
+VALIDATE(){
+    if [ $? -ne 0 ]
+then
+echo "ERROR::Install  not success"
+exit 12
+else
+echo "SUCCESS::install success"
+fi
+}
+
+
+if [ $ID -ne 0 ]
+then
+echo "INSTALL with the ROOT user"
+exit 1
+else
+echo "You are a root user"
+fi
+
+yum install mysql -y
+
+VALIDATE
+
+
