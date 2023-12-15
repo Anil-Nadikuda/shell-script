afdfa424670763856ebfd197309cc454d7d37b88
Anil-Nadikuda <anilnadikuda1430@gmail.com>
2023-12-15 18:49:25 +0530

    changes done

--- 7.logs2.sh
+++ 7.logs2.sh
@@ -4,10 +4,10 @@ ID=$(id -u)
 
 if [ $ID -ne 0 ]
 then
-    echo -e ""\e[31m" ERROR: Run command with root user"
+    echo -e "\e[31m ERROR: Run command with root user"
 exit 1
 else
-    echo "\e[32m logged with root user"
+    echo -e "\e[32m logged with root user"
 fi
 
 yum install mysql -y
