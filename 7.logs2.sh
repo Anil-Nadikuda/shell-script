0a7194ebfb1c8b948090df835cb4b028c55b303b
Anil-Nadikuda <anilnadikuda1430@gmail.com>
2023-12-15 18:57:21 +0530

    changes done

--- 7.logs2.sh
+++ 7.logs2.sh
@@ -5,6 +5,16 @@ R="\e[31m"
 G="\e[32m"
 N="\e[0m"
 
+VALIDATE(){
+    if [ $1 -ne 0 ]
+then
+echo "ERROR::$2.... Failed"
+exit 12
+else
+echo "SUCCESS::$2.... Success"
+fi
+}
+
 if [ $ID -ne 0 ]
 then
     echo -e "$R ERROR: Run command with root user $N"
@@ -14,4 +24,4 @@ else
 fi
 
 yum install mysql -y
-VALIDATE $? " mysql"
+VALIDATE $? "mysql"
