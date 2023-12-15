242c0fde0392533b44416e14cdbd967cc05c820e
Anil-Nadikuda <anilnadikuda1430@gmail.com>
2023-12-15 18:43:29 +0530

    changes done

--- 7.logs2.sh
+++ 7.logs2.sh
@@ -4,7 +4,7 @@ ID=$(id -u)
 
 if [ $ID -ne 0 ]
 then
-    echo -e "\e[31m ERROR: Run command with root user"
+    echo -e ""\e[31m" ERROR: Run command with root user"
 exit 1
 else
     echo "\e[32m logged with root user"
